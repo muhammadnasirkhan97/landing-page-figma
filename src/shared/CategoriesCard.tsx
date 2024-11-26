f3fa8bbd75c9231a8f2ccca290faffa2b8deecf3
muhammad nasir khan <khanmuhammadnasir9977@gmail.com>
2024-11-26 23:10:22 +0500

    landing page figma

---
+++ src/shared/CategoriesCard.tsx
@@ -0,0 +1,11 @@
+"use client"
+
+const CategoriesCard = () => {
+  return (
+    <div>
+       Category Card
+    </div>
+  )
+}
+
+export default CategoriesCard
---
+++ src/shared/ProductCards.tsx
@@ -0,0 +1,12 @@
+"use client"
+
+
+const ProductCards = () => {
+  return (
+    <div>
+      Product Cards
+    </div>
+  )
+}
+
+export default ProductCards
